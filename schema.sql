eaa2a4db77f35a13772060fb6b054a53b2cfc073
maxthestranger <thestranger@gmail.com>
2022-05-31 10:37:48 +0300

    add solution

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int primary key not null,
+    name varchar(100),
+    date_of_birth datetime,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal
 );
