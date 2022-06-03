6baccfab0ff60891bf6c0d592e93f54b1315c45d
maxthestranger <thestrangermax@gmail.com>
2022-06-03 16:10:05 +0300

    add queries

--- queries.sql
+++ queries.sql
@@ -7,3 +7,36 @@ SELECT name, date_of_birth FROM animals WHERE weight_kg >= 10.5;
 SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'pokimon' WHERE species IS NULL;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
+SAVEPOINT date_save_point;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO date_save_point;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT count(*) FROM animals;
+SELECT count(*) FROM animals WHERE escape_attempts = 0;
+SELECT avg(weight_kg) FROM animals;
+SELECT * FROM animals WHERE escape_attempts = (SELECT max(escape_attempts) FROM animals);
+SELECT max(weight_kg) FROM animals GROUP BY name;
+SELECT avg(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY name;
